c8a8335ffd46eda75ccd0d37fbc03eff81756fb0
satan-nightmare <sumitsinghsakhwar@gmail.com>
2018-03-17 12:38:16 +0530

    imrovements in Database

--- src/application/Database.java
+++ src/application/Database.java
@@ -13,7 +13,7 @@ public class Database {
 
     public Database() {
         try {
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sam", "root", "");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sam", "root", "steve");
             System.out.println("Connection to the database has been established!!");
             prev_id = -1;
         } catch (SQLException e) {
